6173c929b03a3123ee6d6de5d4620bbc8aba9d09
lucasnog181 <lucasnog17@edu.unifor.br>
2021-06-20 15:24:31 -0300

    Commit 1.0

---
+++ src/components/header/header.data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: 'home',
+    label: 'Home',
+  },
+  {
+    path: 'feature',
+    label: 'Features',
+  },
+  {
+    path: 'contact',
+    label: 'Contact',
+  },
+  {
+    path: 'aboutus',
+    label: 'About us',
+  },
+];
